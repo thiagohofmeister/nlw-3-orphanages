63677d0f33b7fa805a61659918d7c5cb0503f888
Thiago Hofmeister <thiago.souza@moovin.com.br>
2020-10-14 23:36:43 -0300

    Adiciona css do leaflet

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import './styles/global.scss'
+import 'leaflet/dist/leaflet.css'
 import Routes from './routes'
 
 function App () {
   return (
     <Routes />
   )
+
 }
 
 export default App
